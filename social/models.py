67498806b0a3b0f3fd34e6528523d62586da495c
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-06-13 23:20:47 +0300

    add Post model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    body = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
